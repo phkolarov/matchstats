b2005407c56582d33cfae391830836afeff287cb
EUROHOLD\Philip.Kolarov <philip.kolarov@itsoft.bg>
2019-02-15 21:17:38 +0200

    commit

--- public/js/matchstats_page_files/matchstats_common_data_page.js
+++ public/js/matchstats_page_files/matchstats_common_data_page.js
@@ -9,23 +9,21 @@ $(document).ready(function () {
     function data_table_initialisation(columns_array) {
 
         $(document).ready(function () {
-            // Setup - add a text input to each footer cell
-            $('#data tfoot th').each(function () {
-                var title = $(this).text();
-                $(this).html('<input style="width: 30px" type="text" placeholder="' + title + '" />');
-            });
-
 
             let columns = [];
-
-            for(let column in columns_array){
+            for (let column in columns_array) {
 
                 let column_name = columns_array[column];
                 let visible = localStorage[column_name] == "false" ? false : true;
 
                 columns.push({
                     'data': columns_array[column],
-                    'visible': visible
+                    'visible': visible,
+                    'className': column_name
+                    // "createdCell": function (td, cellData, rowData, row, col) {
+                    //
+                    //     $(td).attr('type',column_name);
+                    // }
                 })
             }
 
@@ -43,20 +41,21 @@ $(document).ready(function () {
                         }
                     },
                     rowCallback: function (row, data) {
-                        $(row).attr('row-id',data.id);
+
+                        $(row).attr('row-id', data.id);
                     },
                     "columns": columns
                 }
             );
 
             //adding multiselection
-            $('#data tbody').on( 'click', 'tr', function () {
+            $('#data tbody').on('click', 'tr', function () {
                 $(this).toggleClass('selected');
-            } );
+            });
 
-            $('#button').click( function () {
-                alert( table.rows('.selected').data().length +' row(s) selected' );
-            } );
+            $('#button').click(function () {
+                alert(table.rows('.selected').data().length + ' row(s) selected');
+            });
 
             // Apply the search
             table.columns().every(function () {
@@ -70,9 +69,76 @@ $(document).ready(function () {
                     }
                 });
             });
+
+            // Setup - add a text input to each footer cell
+            $('#data tfoot th').each(function (i, el) {
+                var title = $(this).text();
+                let width = 33;
+
+                console.log(title);
+                if (i == 0) {
+                    width = 61;
+                }
+                $(this).html(`<input style="width: ${width}px" type="text" placeholder="${title}" />`);
+            });
         });
     }
 
+
+    function insert_data_row() {
+
+        $('#insertDataRow').on('click', function () {
+
+            let columns = $('#data>thead>tr th');
+            let date = new Date();
+            let add_row_button = `<button insertion-button-id="${date.getTime()}" class="btn btn-xs btn-success add-row-button add-row-buttons" time="${date.getTime()}">ADD</button>`;
+            let cancel_row_button = `<button insertion-button-id="${date.getTime()}" class="btn btn-xs btn-default cancel-row-button add-row-buttons" time="${date.getTime()}">CANCEL</button>`;
+            let row_string = `<tr class="insertion_row" insertion-row-id="${date.getTime()}">`;
+
+            columns.each(function (i, el) {
+                let column_name = $(el).attr('column_type');
+                let add_button_string = '';
+                let cancel_button_string = '';
+                if (column_name == 'id') {
+                    add_button_string = add_row_button;
+                    cancel_button_string = cancel_row_button;
+                }
+
+                row_string += `
+                    <td>${add_button_string}<input time="${date.getTime()}" style="width: 60px" type="text" placeholder="${column_name}">${cancel_button_string}</td>
+                `;
+            });
+            row_string += '</tr>';
+
+            $('#data tbody').prepend(
+                row_string
+            );
+        })
+
+        $(document).on('click', '.add-row-button', function () {
+            let time = $(this).attr('insertion-button-id');
+            let inputs = $(`input[time="${time}"]`);
+            let data = {};
+            inputs.each(function () {
+                let name = $(this).attr('placeholder');
+                let value = $(this).val();
+                data[name] = value;
+            });
+
+            if(!data.home_team || !data.away_team ){
+                match_app.message('Please enter team names','error');
+                return;
+            }
+
+            match_app.data_service('/')
+        });
+        $(document).on('click', '.cancel-row-button', function () {
+
+            let id = $(this).attr('insertion-button-id');
+            $(`[insertion-row-id="${id}"]`).remove();
+        })
+
+    }
 
     function upload_file() {
         // UPLOAD FILE
@@ -156,20 +222,22 @@ $(document).ready(function () {
 
         $('#save_show_hide_columns').on('click', function () {
             let columns = $('.column-option')
-            for(let column of columns){
-                localStorage[$(column).attr('id')] = $(column).is(':checked') ? true: false;
+            for (let column of columns) {
+                if ($(column).attr('id') != 'id') {
+                    localStorage[$(column).attr('id')] = $(column).is(':checked') ? true : false;
+                }
             }
 
             $('#showHideColumnsModal').modal('hide');
             setTimeout(function () {
                 location.reload();
-            },2000)
+            }, 2000)
         })
     }
 
     function load_columns_data() {
 
-      return match_app.request('system/get-columns', 'GET', [], [], function (data) {
+        return match_app.request('system/get-columns', 'GET', [], [], function (data) {
 
             let columns = data.success;
             let element = `<tr>`;
@@ -177,7 +245,10 @@ $(document).ready(function () {
                 let column_name = columns[column];
                 let checked = '';
 
-                if(localStorage[column_name] == "true" || localStorage[column_name] == undefined){
+                if (column_name == 'id') {
+                    continue;
+                }
+                if (localStorage[column_name] == "true" || localStorage[column_name] == undefined) {
                     checked = 'checked';
                 }
 
@@ -185,7 +256,7 @@ $(document).ready(function () {
                                     <td><label for="${column_name}">${column_name}</label>
                                     <td><input id="${column_name}" type="checkbox" class="column-option" ${checked}></td>
                 `
-                if (column % 2 != 0 || (columns.length - 1) == column) {
+                if (column % 2 == 0 || (columns.length - 1) == column) {
                     element += ' </tr>';
                     $('#showHideOptionsTable').append(element)
                     element = '<tr>'
@@ -196,8 +267,6 @@ $(document).ready(function () {
         })
 
 
-
-
     }
 
     load_columns_data().then(function (data) {
@@ -206,5 +275,6 @@ $(document).ready(function () {
 
     upload_file();
     show_hide_columns();
+    insert_data_row();
 
 });
--- resources/views/matchstats.blade.php
+++ resources/views/matchstats.blade.php
@@ -25,49 +25,47 @@
                                style="width:100%">
                             <thead class="thead-dark">
                             <tr class="">
-                                <th scope="col">ID</th>
-                                <th scope="col">DATE</th>
-                                <th scope="col">TIME</th>
-                                <th scope="col">HOME TEAM</th>
-                                <th scope="col">AWAY TEAM</th>
-                                <th scope="col">CNTRY</th>
-                                <th scope="col">DVSN</th>
-                                <th scope="col">STAGE</th>
-                                <th scope="col">PLOF</th>
-                                <th scope="col">ELIMI</th>
-                                <th scope="col">SEASO</th>
-                                <th scope="col">HTPO2,5</th>
-                                <th scope="col">HTPHSR</th>
-                                <th scope="col">HTPTSR</th>
-                                <th scope="col">HTRH</th>
-                                <th scope="col">HTRT</th>
-                                <th scope="col">FTRF</th>
-                                <th scope="col">FTRT</th>
-                                <th scope="col">ATPTSR</th>
-                                <th scope="col">ATPASR</th>
-                                <th scope="col">ATPO2,5</th>
-                                <th scope="col">NS</th>
-                                <th scope="col">HTTW</th>
-                                <th scope="col">DRAW</th>
-                                <th scope="col">ATTW</th>
-                                <th scope="col">HTWOD</th>
-                                <th scope="col">ATWOD</th>
-                                <th scope="col">HTOATW</th>
-                                <th scope="col">HTDNB</th>
-                                <th scope="col">ATDNB</th>
-                                <th scope="col">HTO 0,5</th>
-                                <th scope="col">HTO 1,5</th>
-                                <th scope="col">HTO 2,5</th>
-                                <th scope="col">ATO 0,5</th>
-                                <th scope="col">ATO 1,5</th>
-                                <th scope="col">ATO 2,5</th>
-                                <th scope="col">BTTS</th>
-                                <th scope="col">TGO 0,5</th>
-                                <th scope="col">TGO 1,5</th>
-                                <th scope="col">TGO 2,5</th>
-                                <th scope="col">TGO 3,5</th>
-
-
+                                <th column_type="id" scope="col">ID</th>
+                                <th column_type="date" scope="col">DATE</th>
+                                <th column_type="time" scope="col">TIME</th>
+                                <th column_type="country" scope="col">HOME TEAM</th>
+                                <th column_type="division" scope="col">AWAY TEAM</th>
+                                <th column_type="stage" scope="col">CNTRY</th>
+                                <th column_type="play_offs" scope="col">DVSN</th>
+                                <th column_type="eliminations" scope="col">STAGE</th>
+                                <th column_type="season" scope="col">PLOF</th>
+                                <th column_type="home_team_percent_over_2_5_goals" scope="col">ELIMI</th>
+                                <th column_type="home_team_percent_home_success_rate" scope="col">SEASO</th>
+                                <th column_type="home_team_percent_total_success_rate" scope="col">HTPO2,5</th>
+                                <th column_type="home_team" scope="col">HTPHSR</th>
+                                <th column_type="half_time_result_h" scope="col">HTPTSR</th>
+                                <th column_type="half_time_result_t" scope="col">HTRH</th>
+                                <th column_type="full_time_result_f" scope="col">HTRT</th>
+                                <th column_type="full_time_result_t" scope="col">FTRF</th>
+                                <th column_type="away_team" scope="col">FTRT</th>
+                                <th column_type="away_team_percent_total_success_rate" scope="col">ATPTSR</th>
+                                <th column_type="away_team_percent_away_success_rate" scope="col">ATPASR</th>
+                                <th column_type="away_team_percent_over_2_5_goals" scope="col">ATPO2,5</th>
+                                <th column_type="neutral_stadium" scope="col">NS</th>
+                                <th column_type="home_team_to_win" scope="col">HTTW</th>
+                                <th column_type="draw" scope="col">DRAW</th>
+                                <th column_type="away_team_to_win" scope="col">ATTW</th>
+                                <th column_type="home_team_win_or_draw" scope="col">HTWOD</th>
+                                <th column_type="away_team_win_or_draw" scope="col">ATWOD</th>
+                                <th column_type="home_team_or_away_team_win" scope="col">HTOATW</th>
+                                <th column_type="home_team_draw_no_bet" scope="col">HTDNB</th>
+                                <th column_type="away_team_draw_no_bet" scope="col">ATDNB</th>
+                                <th column_type="home_team_over_0_5_goals" scope="col">HTO 0,5</th>
+                                <th column_type="home_team_over_1_5_goals" scope="col">HTO 1,5</th>
+                                <th column_type="home_team_over_2_5_goals" scope="col">HTO 2,5</th>
+                                <th column_type="away_team_over_0_5_goals" scope="col">ATO 0,5</th>
+                                <th column_type="away_team_over_1_5_goals" scope="col">ATO 1,5</th>
+                                <th column_type="away_team_over_2_5_goals" scope="col">ATO 2,5</th>
+                                <th column_type="both_teams_to_score" scope="col">BTTS</th>
+                                <th column_type="total_goals_over_0_5" scope="col">TGO 0,5</th>
+                                <th column_type="total_goals_over_1_5" scope="col">TGO 1,5</th>
+                                <th column_type="total_goals_over_2_5" scope="col">TGO 2,5</th>
+                                <th column_type="total_goals_over_3_5" scope="col">TGO 3,5</th>
                                 {{--                                    <th class="w-20">id</th>--}}
                                 {{--                                    <th class="w-20">date</th>--}}
                                 {{--                                    <th class="w-15">time</th>--}}
